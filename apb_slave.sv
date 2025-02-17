4d0768dc275d6cae8af258d7ab719fa6dd7cb1a3
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-02-17 13:14:48 -0800

    Update apb_design.sv

--- apb_slave.sv
+++ apb_slave.sv
@@ -14,6 +14,59 @@ module apb_slave(
 
  logic [31:0] mem [0:255];//256 depth memory element with 32 bit data size.
 
+ typedef enum logic [1:0] {IDLE,SETUP,ACCESS,ACCESS_WAIT}state; 
+ state ns,cs;
 
+ always_ff@(posedge PCLK or negedge PRESETn)
+  begin
+   if(!PRESETn)
+    cs <= IDLE;
+   else
+    cs <= ns;
+  end
+
+ //NEXT STATE LOGIC
+ always_comb
+  begin
+   ns = IDLE;
+   unique case(cs)
+    IDLE   : begin
+              if(PSEL)
+               ns = SETUP;
+              else
+               ns = ACCESS;
+             end
+    SETUP  : begin
+              ns = ACCESS;
+             end
+    ACCESS : begin
+              if(PREADY)
+               begin
+                if(!PSEL)
+                 ns = IDLE;
+                else
+                 ns = SETUP;
+               end
+              else
+               ns = ACCESS_WAIT;
+             end    
+   endcase
+  end
+
+ always_ff@(posedge CLK, negedge PRESETn)
+  begin
+   if(!PRESETn)
+    PRDATA <= 0;
+   else
+    begin
+     if(cs==ACCESS)
+      begin
+       if(PWRITE)
+         mem[PADDR] <= PWDATA;
+       else
+        PRDATA <= mem[PADDR];
+      end
+    end
+  end
 
 endmodule
