83a8b264f7ecaf0419cbf8b532ad8ad0cf23acf8
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-02-17 19:29:01 -0800

    Create apb_gen.sv

---
+++ apb_gen.sv
@@ -0,0 +1,18 @@
+class apb_gen;
+  rand transaction tr;
+  mailbox gen2drv;
+
+  function new(mailbox gen2drv);
+    this.gen2drv = gen2drv;
+  endfunction
+
+  task main();
+    repeat(10)
+      begin
+        tr = new();
+        tr.randomize();
+        gen2drv.put(tr);
+      end
+    $display(  "Generator Done" );
+  endtask
+endclass
