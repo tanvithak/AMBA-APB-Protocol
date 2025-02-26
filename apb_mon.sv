3eecd0f9e8672bc3371391624262ea7f621bdc55
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-02-25 22:20:55 -0800

    Update apb_mon.sv

--- apb_mon.sv
+++ apb_mon.sv
@@ -1,1 +1,37 @@
+class apb_mon;
+  virtual apb_if vif;
+  mailbox mon2sb;
 
+  function new(virtual apb_if vif, mailbox mon2sb);
+    this.vif = vif;
+    this.mon2sb = mon2sb;
+  endfunction
+
+  task main;
+    forever
+      begin
+        apb_trans tr;
+        tr = new();
+        wait(vif.PRESETn);
+
+        @(posedge vif.MON_MP.PCLK);
+        @(posedge vif.MON_MP.PCLK);
+        tr.PADDR = vif.MON_MP.mon_cb.PADDR;
+        tr.PWRITE = vif.MON_MP.mon_cb.PWRITE;
+        tr.PENABLE = vif.MON_MP.mon_cb.PENABLE;
+        tr.PSEL = vif.MON_MP.mon_cb.PSEL;
+
+        if(vif.MON_MP.mon_cb.PWRITE)
+          tr.PWDATA = vif.MON_MP.mon_cb.PWDATA;
+
+        if(!vif.MON_MP.mon_cb.PWRITE)
+          tr.PRDATA = vif.MON_MP.mon_cb.PRDATA;
+            
+        mon2sb.put(tr);
+
+        $display("Monitor done");
+      end
+  endtask
+
+endclass
+            
