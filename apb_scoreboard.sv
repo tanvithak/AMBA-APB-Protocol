9019b795a8800a9147204fc7c67969d5a3074d2e
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-03-07 00:10:44 -0800

    Update apb_scoreboard.sv

--- apb_scoreboard.sv
+++ apb_scoreboard.sv
@@ -1,20 +1,43 @@
+`include "apb_trans.sv"
+import apb_pkg::*;
 class apb_scoreboard;
- mailbox mon2sb;
+ int trans_count = 0;
+ mailbox #(apb_trans) mon2sb;
  bit [31:0] mem[64];
+ int max_trans;
 
- function new(mailbox mon2sb);
+ function new(mailbox #(apb_trans)mon2sb, int max_trans=100);
   this.mon2sb = mon2sb;
   foreach(mem[i])
     mem[i] = 32'h00;
+  this.max_trans = max_trans;
  endfunction
 
  task main;
-  apb_trans tr;
-  forever
+  $display("[SB] Scoreboard started...");
+  repeat(100)
    begin
-    #50;
+    apb_trans tr;
     mon2sb.get(tr);
-    $display("Scoreboard Done");
+    trans_count++;
+    $display("[SB] Received transaction %0d: PADDR=%h, PWRITE=%b",
+               trans_count, tr.PADDR, tr.PWRITE);
+    if (tr.PWRITE) 
+     begin
+      mem[tr.PADDR] = tr.PWDATA;
+      $display("[SB] WRITE: Stored %h at address %h", tr.PWDATA, tr.PADDR);
+     end 
+    else 
+     begin
+      if (mem[tr.PADDR] !== tr.PRDATA) 
+       $display("[SB] READ MISMATCH at address %h: Expected %h, but got %h",tr.PADDR, mem[tr.PADDR], tr.PRDATA);
+      else 
+       $display("[SB] READ MATCH at address %h: Value = %h", tr.PADDR, tr.PRDATA);
+     end
+      if (trans_count >= max_trans) begin
+        $display("[SB] Reached maximum transactions. Stopping.");
+        $finish;
+     end
    end
  endtask
 
