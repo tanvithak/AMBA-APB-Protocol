a06f3ea9f47a922c56a3e8d43e9ab52a6fc7dd6b
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-03-07 00:11:42 -0800

    Update apb_test.sv

--- apb_test.sv
+++ apb_test.sv
@@ -1,9 +1,31 @@
 `include "apb_env.sv"
-program test(apb_if vif);
- apb_env env;
- initial
-  begin
-   env = new(vif);
-   env.run();
-  end
-endprogram
+
+class test;
+  apb_env env;
+  virtual apb_if vif;
+  int num_trans = 10; // Expected number of transactions
+
+  function new(virtual apb_if vif);
+    this.vif = vif;
+    env = new(vif);
+  endfunction
+
+  task run();
+    $display("[TEST] Simulation started...");
+    env.run();
+
+    fork
+      begin
+        wait(env.sb.trans_count >= num_trans); 
+        $display("[TEST] All transactions completed. Ending simulation.");
+        $finish;
+      end
+
+      begin
+        #5000ns; 
+        $display("[TEST] Simulation Timeout! Ending.");
+        $stop;
+      end
+    join_any;
+  endtask
+endclass
