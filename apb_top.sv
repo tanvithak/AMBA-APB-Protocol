c325158b3e3e25c68af6824748b35910b0350670
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-03-07 00:13:02 -0800

    Update apb_top.sv

--- apb_top.sv
+++ apb_top.sv
@@ -1,21 +1,40 @@
-//`include "apb_test.sv"
+`include "apb_test.sv"
 `include "apb_if.sv"
 
 module top;
- bit clk;
- bit reset;
+  bit clk = 0;  
+  bit reset;
 
- always #5 clk = ~clk;
+  always #5 clk = ~clk; 
 
- initial
-  begin
-   reset = 0;
-   #10 reset = 1;
+  initial begin
+    reset = 0;
+    #20 reset = 1;  
+    $display("[TOP] Reset asserted");
+  end
+ 
+  apb_if a1(clk, reset);
+
+  test t1;
+
+  initial begin
+    $display("[TOP] Starting the test...");
+    t1 = new(a1);
+    t1.run();
+    $display("[TOP] Test completed.");
+    $finish;
   end
 
- apb_if a1(clk,reset);
- test t1(vif);
-
- apb_design dut1(a1.PCLK,a1.PRESETn,a1.PSEL,a1.PENABLE,a1.PWRITE,a1.PADDR,a1.PWDATA,a1.PRDATA,a1.PREADY);
+  apb_design dut1 (
+    .PCLK(a1.PCLK),
+    .PRESETn(a1.PRESETn),
+    .PSEL(a1.PSEL),
+    .PENABLE(a1.PENABLE),
+    .PWRITE(a1.PWRITE),
+    .PADDR(a1.PADDR),
+    .PWDATA(a1.PWDATA),
+    .PRDATA(a1.PRDATA),
+    .PREADY(a1.PREADY)
+  );
 
 endmodule
