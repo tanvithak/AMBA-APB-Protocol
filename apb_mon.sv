68dcea1ca7f795314fe63e7314dc1cccb7c0edfa
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-03-07 00:10:21 -0800

    Update apb_mon.sv

--- apb_mon.sv
+++ apb_mon.sv
@@ -1,37 +1,38 @@
+`include "apb_trans.sv"
+import apb_pkg::*;
 class apb_mon;
   virtual apb_if vif;
-  mailbox mon2sb;
+  mailbox #(apb_trans) mon2sb;
 
-  function new(virtual apb_if vif, mailbox mon2sb);
+  function new(virtual apb_if vif, mailbox #(apb_trans) mon2sb);
     this.vif = vif;
     this.mon2sb = mon2sb;
   endfunction
 
   task main;
-    forever
-      begin
-        apb_trans tr;
-        tr = new();
-        wait(vif.PRESETn);
+    apb_trans tr; 
+    $display("[MON] Monitor started...");
+    
+    repeat(100) begin
+      tr = new();
+      wait(vif.PRESETn);
 
-        @(posedge vif.MON_MP.PCLK);
-        @(posedge vif.MON_MP.PCLK);
-        tr.PADDR = vif.MON_MP.mon_cb.PADDR;
-        tr.PWRITE = vif.MON_MP.mon_cb.PWRITE;
-        tr.PENABLE = vif.MON_MP.mon_cb.PENABLE;
-        tr.PSEL = vif.MON_MP.mon_cb.PSEL;
+      @(posedge vif.MON_MP.PCLK);
+      @(posedge vif.MON_MP.PCLK);
+      
+      tr.PADDR   = vif.MON_MP.mon_cb.PADDR;
+      tr.PWRITE  = vif.MON_MP.mon_cb.PWRITE;
+      tr.PENABLE = vif.MON_MP.mon_cb.PENABLE;
+      tr.PSEL    = vif.MON_MP.mon_cb.PSEL;
 
-        if(vif.MON_MP.mon_cb.PWRITE)
-          tr.PWDATA = vif.MON_MP.mon_cb.PWDATA;
-
-        if(!vif.MON_MP.mon_cb.PWRITE)
-          tr.PRDATA = vif.MON_MP.mon_cb.PRDATA;
-            
-        mon2sb.put(tr);
-
-        $display("Monitor done");
-      end
+      if (vif.MON_MP.mon_cb.PWRITE)
+        tr.PWDATA = vif.MON_MP.mon_cb.PWDATA;
+      else
+        tr.PRDATA = vif.MON_MP.mon_cb.PRDATA;
+          
+      mon2sb.put(tr);
+      $display("[MON] Captured: PADDR=%h, PWRITE=%b, PWDATA=%b, PRDATA = %b", tr.PADDR, tr.PWRITE, tr.PWDATA, tr.PRDATA);
+    end
   endtask
 
 endclass
-            
