f140482b21154f8f0980f38f34dca1d2bb8804f7
tanvithak <150294510+tanvithak@users.noreply.github.com>
2025-03-07 00:09:03 -0800

    Update apb_gen.sv

--- apb_gen.sv
+++ apb_gen.sv
@@ -1,18 +1,25 @@
-class apb_gen;
-  rand apb_trans tr;
-  mailbox gen2drv;
+`include "apb_trans.sv"
+import apb_pkg::*;
 
-  function new(mailbox gen2drv);
+class apb_gen;
+  apb_trans tr;
+  mailbox #(apb_trans) gen2drv;
+
+  function new(mailbox #(apb_trans) gen2drv);
     this.gen2drv = gen2drv;
   endfunction
 
   task main();
-    repeat(10)
+    $display("[GEN] Generator started...");
+    repeat(100)
       begin
         tr = new();
         tr.randomize();
+        if(!tr.randomize())
+         $fatal("Generator : Randomization failed");
         gen2drv.put(tr);
+        $display("[GEN] Sent transaction: PADDR=%h, PWRITE=%b", tr.PADDR, tr.PWRITE);
       end
-    $display(  "Generator Done" );
+    $display("[GEN] Generator Done");
   endtask
 endclass
